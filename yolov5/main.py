a6c51663669adf66a5c84b08ad5974f7b2fa792d
BraedenCu <braedencullen120@gmail.com>
2021-06-12 11:47:49 -0500

    adjusted for id changes

--- yolov5/main.py
+++ yolov5/main.py
@@ -4,12 +4,11 @@ from threading import Thread
 import multiprocessing
 import argparse
 import time
+#detectFunc.py requirements
 from pathlib import Path
-
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
-
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages, LoadFromRealsense
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
@@ -17,6 +16,13 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 from detectFunc import detect
+#vexaiBrainComm requirements
+import array
+import serial
+import struct
+import time
+import zlib
+import vexaiBrainComm 
 
 #is this needed?
 @torch.no_grad()
@@ -33,6 +39,13 @@ def consumer(in_q):
     while True:
         #get data
         data = in_q.get()
+        if data[0] != None:
+            #params: x, y, width, height, 
+            x = vexaiBrainComm.FifoObjectBoxType(0)
+            x.x = data[0][0]
+            x.y = data[0][1]
+            x.depth = data[1][0]
+            x.classId = data[2][0]
         #process data
         print(data)
    
