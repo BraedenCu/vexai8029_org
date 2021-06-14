aa14a6d4445dc4a85b88685bc0dc0e57f475b1e2
BraedenCu <braedencullen120@gmail.com>
2021-06-13 19:16:18 -0500

    added cupy version of detect.py

---
+++ yolov5/detectFuncGPU.py
@@ -0,0 +1,216 @@
+import argparse
+from os import close
+import time
+from pathlib import Path
+import cupy as cp
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages, LoadFromRealsense
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
+from utils.plots import colors, plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
+@torch.no_grad()
+def detect(source, weights, view_img, imgsz, queue):
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/pp3/weights/last.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum number of detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--viewimg', type=int, help='display results', default=0)
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    opt = parser.parse_args()
+    
+    source, weights, view_img, save_txt, imgsz = '0', weights, view_img, False, 640
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    #maximum number of detections per image = 1000
+    #conf-thres, iou-thres, max-det, device = 0.25, 0.45, 1000, ''
+    
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+    view_img = True if view_img == 1 else False
+    
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = opt.half and device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    #stride = int(model.stride.max())  # model stride
+    stride = int(32)
+    #imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    imgsz = 640
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        #view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        #dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+        dataset = LoadFromRealsense(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    t0 = time.time()
+    
+    for path, img, im0s in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms,
+                                   max_det=opt.max_det)
+        t2 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        idArr = []
+        centerArr = []
+        depthArr = []
+        # detections per image
+        for i, det in enumerate(pred):  
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path, f'{i}: ', im0s, dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+     
+            #imc = im0.copy() if opt.save_crop else im0  # for opt.save_crop
+            imc = im0
+            
+            #if something is detected run the following    
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                
+                # Print results
+                #for c in det[:, -1].unique():
+                #    n = (det[:, -1] == c).sum()  # detections per class
+                #    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string            
+                
+                cupyArray = cp.asarray(det)
+                detectionIDsNumpy = cupyArray[:, -1].numpy()
+                i = detectionIDsNumpy.size - 1
+                
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    #add id of detected object to array
+                    idArr.append(detectionIDsNumpy[i])
+                    
+                    # Add bbox to image
+                    if save_img or opt.save_crop or view_img or True:  # Add bbox to image
+                        # Calculate depth
+                        # Splitting xyxy* (measurement)
+                        xmin = int(xyxy[0])
+                        ymin = int(xyxy[1])
+                        xmax = int(xyxy[2])
+                        ymax = int(xyxy[3])
+                        # Calculate width and height
+                        w = xmax - xmin
+                        h = ymax - ymin
+
+                        # Calculating measured centroid of the object (in Pixel)
+                        xc = int(round(((xmax + xmin) / 2), 0))
+                        yc = int(round(((ymax + ymin) / 2), 0))
+                        #depth_pixel = [xc, yc]
+                        #xc_msr = float((xyxy[2] + xyxy[0])/2)
+                        #yc_msr = float((xyxy[3] + xyxy[1])/2)
+                        #meas_pixel = [xc_msr, yc_msr]
+                        
+                        #append depth in meters to center point
+                        depthArr.append(dataset.getdepth(xc, yc)) 
+                        centerArr.append([xc, yc])
+                        
+                        c = int(cls)  # integer class
+                        label = None if opt.hide_labels else (names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
+                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=opt.line_thickness)
+                        if opt.save_crop:
+                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                    
+                    i-=1
+                
+                #determine which ball is the closest
+                closest = depthArr[0]    
+                closestIndex = 0
+                for o in range(1, len(depthArr)-1):
+                    if depthArr[o] <= closest:
+                        closestIndex = o
+                        closest = depthArr[o]
+                    
+                #add the closest ball to the queue. Add the location of the center of the ball, the depth to the center of the ball, and the id of the ball
+                queue.put([centerArr[closestIndex], depthArr[closestIndex], idArr[closestIndex]])
+                    
+                    
+            # Print time (inference + NMS)
+            #print(f'{s}Done. ({t2 - t1:.3f}s)')
+            
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str("hehe"), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            #print(ballArr)
+            #print(centerArr)
+            #print(depthArr)
+            #print('\n')
+            
+            
+            # Stream results
+            if view_img:
+                cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('RealSense', im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+    
+    #print(f'Done. ({time.time() - t0:.3f}s)')
