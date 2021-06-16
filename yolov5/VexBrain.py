2097756a79af85198807bb8dc0d90f3957659e52
BraedenCu <braedencullen120@gmail.com>
2021-06-15 19:27:41 -0500

    logging

--- yolov5/VexBrain.py
+++ yolov5/VexBrain.py
@@ -676,18 +676,18 @@ class VexBrain:
                     self.msgRxCnt += 1
                     #logging.info("RX : %04d %s", self.msgRxCnt, data)
                     #if (self.msgRxCnt % 10) == 0:
-                    #if (self.msgRxCnt % 1) == 0:
-                    if self.detectInfo != None:
-                        self.createMsgFromDetectInfo()
-                        #self.mpt.printVerbose()
-                        #self.detectInfo.displayBrief()
-                        packedMsg = self.mpt.getPackedMsg()
-                        #if lastMsg != packedMsg:
-                        #    logging.info("TX : %s", packedMsg.hex())
-                        self.brain.write(packedMsg)
-                        self.msgTxCnt += 1
-                        lastMsg = packedMsg
-                        #break
+                    if (self.msgRxCnt % 1) == 0:
+                        if self.detectInfo != None:
+                            self.createMsgFromDetectInfo()
+                            #self.mpt.printVerbose()
+                            #self.detectInfo.displayBrief()
+                            packedMsg = self.mpt.getPackedMsg()
+                            #if lastMsg != packedMsg:
+                            #    logging.info("TX : %s", packedMsg.hex())
+                            self.brain.write(packedMsg)
+                            self.msgTxCnt += 1
+                            lastMsg = packedMsg
+                            #break
             except:
                 break
 
