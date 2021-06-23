39a5d799c56d7a2f8e2bcbc70ae95436005977a3
BraedenCu <braedencullen120@gmail.com>
2021-06-22 20:52:52 -0500

    improve

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# chkconfig: 345 99 10
+# Description: auto start apex listener
+#
 
 sudo docker run --gpus all -it --privileged -v /dev:/dev -v /home/nano/Development/vexai8029_org/:/usr/shared-dev yolov5:1.0
